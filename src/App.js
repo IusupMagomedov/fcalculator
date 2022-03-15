06a69867bd6b0046b705dbea9c13220b2480b6b5
IusupMagomedov <74495487+IusupMagomedov@users.noreply.github.com>
2022-03-15 15:27:04 +0300

    Added math symbols

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-
+import {useState} from 'react'
 import './App.css';
-const handleClick = event => console.log(event.target.id)
+
+const digits = {
+  'zero': 0,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9}
+
 function App() {
+  const [displayLable, setDisplayLable] = useState(0)
+
+  const handleClick = event => {
+    switch(event.target.id) {
+      case "clear":
+        setDisplayLable(0)
+        break
+      // case "one":
+      //   setDisplayLable(String(displayLable) + 1)
+      //   break
+    }
+    digits[event.target.id] && setDisplayLable(String(displayLable) + digits[event.target.id])
+  }
   return (
     <div className="App">
       <button id="equals" onClick={handleClick}>=</button>
@@ -15,6 +40,14 @@ function App() {
       <button id="seven" onClick={handleClick}>7</button>
       <button id="eight" onClick={handleClick}>8</button>
       <button id="nine" onClick={handleClick}>9</button>
+      <button id="add" onClick={handleClick}>+</button>
+      <button id="subtract" onClick={handleClick}>-</button>
+      <button id="multiply" onClick={handleClick}>*</button>
+      <button id="divide" onClick={handleClick}>/</button>
+      <button id="decimal" onClick={handleClick}>.</button>
+      <button id="clear" onClick={handleClick}>clear</button>
+      <lable id="display" >{displayLable}</lable>
+
     </div>
   );
 }
