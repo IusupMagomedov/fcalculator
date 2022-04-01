9a20d50e34f18bcaabf591ab59a1cdcd28d33a70
IusupMagomedov <74495487+IusupMagomedov@users.noreply.github.com>
2022-04-01 12:22:26 +0300

    ended project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
 
 const digits = {
@@ -11,21 +11,123 @@ const digits = {
   'six': 6,
   'seven': 7,
   'eight': 8,
-  'nine': 9}
-
+  'nine': 9, 
+  'decimal': '.'
+}
+  const calculate = (num1, num2, operation) => {
+    let res = 0
+    num1 = Number(num1)
+    num2 = Number(num2)
+    switch (operation){
+      case "add":
+        res = num1 + num2
+        break
+      case "subtract":
+        res = num1 - num2
+        break
+      case "multiply":
+        res = num1 * num2
+        break
+      case "divide":
+        res = num1 / num2
+        break
+      case '':
+        res = num2
+    }
+    console.log(res)
+    return res
+  }
+  const negativeSwitch = str => str === '' ? '-' : ''
+  
 function App() {
   const [displayLable, setDisplayLable] = useState(0)
+  const [operationMemory, setOperationMemory] = useState('')
+  const [numberMemory, setNumberMemory] = useState(0)
+  const [chainFlag, setChainFlag] = useState(false)
+  const [subtractFlag, setsubtractFlag] = useState(false)
+  const [negative, setNegative] = useState('')
+  const [previousButton, setPreviousButton] = useState('')
 
   const handleClick = event => {
+    setPreviousButton(event.target.id)
     switch(event.target.id) {
       case "clear":
         setDisplayLable(0)
+        setNumberMemory(0)
+        setOperationMemory('')
+        setNegative('')
         break
-      // case "one":
-      //   setDisplayLable(String(displayLable) + 1)
-      //   break
+      case "zero":
+        (displayLable != 0) && setDisplayLable(String(displayLable) + digits[event.target.id])
+        setPreviousButton('number')
+        break
+      case "one":
+      case "two":
+      case "three":
+      case "four":
+      case "five":
+      case "six":
+      case "seven":
+      case "eight":
+      case "nine":
+        //  && 
+        (displayLable == 0) || previousButton !== 'number'
+        ? setDisplayLable(negative + digits[event.target.id])
+        : setDisplayLable(negative + String(displayLable) + digits[event.target.id])
+        negative === '-' && setNumberMemory(displayLable)
+        previousButton === 'operation' && setNumberMemory(displayLable)
+
+        setNegative('')
+        setPreviousButton('number')
+        // if(previousButton === "add" || previousButton === "subtract" || previousButton === "multiply" || previousButton === "divide") {
+        //   setDisplayLable(calculate(numberMemory, negative + displayLable, operationMemory))
+        //   break
+        // } 
+        
+        break
+      case "decimal":
+        console.log(/\./.test(displayLable))
+        !/\./.test(displayLable) && setDisplayLable(String(displayLable) + digits[event.target.id])
+        setPreviousButton('number')
+        break
+      case "add":
+      case "subtract":
+      case "multiply":
+      case "divide":
+        if(previousButton === 'operation' && event.target.id === 'subtract') {
+          setNegative(negativeSwitch(negative))
+        } else if(previousButton === 'subtract') {
+          setOperationMemory(event.target.id)
+          setPreviousButton('operation')
+          setNegative('')
+        } else {
+          setOperationMemory(event.target.id)
+          setPreviousButton('operation')
+          setDisplayLable(calculate(numberMemory, displayLable, operationMemory))
+          setNegative('')
+        }
+        
+      
+      // setNegative('')
+        // if(previousButton !== "add" && previousButton !== "subtract" && previousButton !== "multiply" &&previousButton !== "divide") {
+        //   setOperationMemory(event.target.id)
+        //   setNumberMemory(displayLable)
+        //   setDisplayLable(0)
+        // } else if((previousButton === "add" || previousButton === "divide" || previousButton === "multiply") && event.target.id === 'subtract') {
+        //   setNegative('-')
+        // } else {
+        //   setOperationMemory(event.target.id)
+        // }
+        
+        break
+      case "equals":
+        operationMemory !== '' && setDisplayLable(calculate(numberMemory, displayLable, operationMemory)) 
+        setOperationMemory('')
+        setsubtractFlag(false)
+        setPreviousButton('equals')
+        break
     }
-    digits[event.target.id] && setDisplayLable(String(displayLable) + digits[event.target.id])
+  
   }
   return (
     <div className="App">
@@ -46,8 +148,13 @@ function App() {
       <button id="divide" onClick={handleClick}>/</button>
       <button id="decimal" onClick={handleClick}>.</button>
       <button id="clear" onClick={handleClick}>clear</button>
-      <lable id="display" >{displayLable}</lable>
-
+      <div id="display" >{displayLable}</div>
+      <p>operationMemory {operationMemory}</p>
+      <p>numberMemory {numberMemory}</p>
+      <p>chainFlag {chainFlag}</p>
+      <p>negative {negative}</p>
+      <p>subtractFlag {subtractFlag}</p>
+      <p>previousButton {previousButton}</p>
     </div>
   );
 }
